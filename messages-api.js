7722387828ad3a95323450a3e4e9e5a2ae3810ac
gergokutu <gergo.kutu@gmail.com>
2019-08-03 10:46:13 +0200

    text property check implemented

--- messages-api.js
+++ messages-api.js
@@ -6,7 +6,13 @@ const port = process.env.PORT || 3000
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
 
-app.post('/messages', (req, res, next) => res.json(req.body.text))
+app.post('/messages', (req, res, next) => {
+  if (req.body.text === undefined || req.body.text === "") {
+    res.status(400).send({message: 'Text property is missing or empty'})
+  }
+  res.json({message: req.body.text})
+  .catch(next)
+})
 
 app.get('/', (req, res) => res.send('Server is up and running'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
