c4433cff0f32b3d48442c7d3cc50b21f3e789496
gergokutu <gergo.kutu@gmail.com>
2019-08-03 12:10:27 +0200

    request limit with middleware function

--- messages-api.js
+++ messages-api.js
@@ -1,4 +1,3 @@
-// const rateLimit = require("express-rate-limit");
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
@@ -7,26 +6,24 @@ const port = process.env.PORT || 3000
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 5, // limit each IP to 5 requests per windowMs
-//   message:
-//     "Too many accounts created from this IP, please try again after an 15 minutes"
-// });
-
-// app.use(limiter);
 const reqCount = []
+const requestCountingMiddleware = (req, res, next) => {
+  if (reqCount.length > 5) {
+    res.status(429).send('Too many requests')
+  } else {
+      reqCount.push(req.body)
+      next()
+  }
+}
 
-app.post('/messages', (req, res, next) => {
+app.use(requestCountingMiddleware)
+
+app.post('/messages', requestCountingMiddleware, (req, res, next) => {
   if (req.body.text === undefined || req.body.text === "") {
     res.status(400).send({ message: 'Text property is missing or empty' })
-  }
-  if (reqCount.length < 5) {
-    reqCount.push(req.body)
+  } else {
     res.json({ message: req.body.text })
-  } 
-  res.status(429).send('Too many requests')
-  .catch(next)
+  }
 })
 
 app.get('/', (req, res) => res.send('Server is up and running'))
