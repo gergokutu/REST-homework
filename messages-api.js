ff1c381b0e3feac3d5d1b71ed49fd8897d1e7eff
gergokutu <gergo.kutu@gmail.com>
2019-08-03 11:11:22 +0200

    rate limiter implemented

--- messages-api.js
+++ messages-api.js
@@ -1,3 +1,4 @@
+const rateLimit = require("express-rate-limit");
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
@@ -6,10 +7,20 @@ const port = process.env.PORT || 3000
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message:
+    "Too many accounts created from this IP, please try again after an hour"
+});
+
+app.use(limiter);
+
 app.post('/messages', (req, res, next) => {
   if (req.body.text === undefined || req.body.text === "") {
     res.status(400).send({message: 'Text property is missing or empty'})
   }
+  console.log('response test', req)
   res.json({message: req.body.text})
   .catch(next)
 })
