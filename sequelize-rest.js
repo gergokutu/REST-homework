dbb5f9703b932c4f4b4ff287c1d62073e7303c0b
gergokutu <gergo.kutu@gmail.com>
2019-08-03 14:15:02 +0200

    app with routes and RESTful actions

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -5,19 +5,99 @@ const db = new Sequelize(databaseUrl)
 const Movie = db.define(
   'movie',
   {
-    title: Sequelize.STRING,
-    yearOfRelease: Sequelize.INTEGER,
-    synopsis: Sequelize.STRING,
-  }
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    yearOfRelease: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    synopsis: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+  },
+  { tableName: 'movies', timestamps: false }
 )
 
 db
-  .sync() // moved out from sync() » { force: true }
-  .then(() => console.log('Database connected'))
-  .then(() => Promise.all([ // Insert 3 new rows
+  .sync({force: true})
+  .then(() => Movie.truncate())
+  .then(() => Promise.all([
     Movie.create({ title: 'Movie1', yearOfRelease: 1984, synopsis: 'Once upon a time...' }),
     Movie.create({ title: 'Movie2', yearOfRelease: 1985, synopsis: 'Once upon a time...' }),
     Movie.create({ title: 'Movie3', yearOfRelease: 1986, synopsis: 'Once upon a time...' })
   ]))
   .catch(console.error)
+
+  const express = require('express')
+  const bodyParser = require('body-parser')
+  const app = express()
+  const port = process.env.PORT || 3000
+  
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
+  
+  app.post(
+    '/',
+    (req, res, next) => {
+      Movie
+        .findOne({ where: { title: req.body.title }})
+        .then((movie) => {
+          if ((movie)) {
+            res.status(403).send("Title already used.")
+          } else {
+            return Movie
+              .create(req.body)
+              .then((newMovie) => res.send((newMovie)))
+          }
+        })
+        .catch(next)
+    }
+  )
+  
+  app.get('/collections', (req, res, next) => {
+    Movie.findAll()
+        .then(movies => {
+            res.json(movies)
+        })
+        .catch(next)
+  })
+  
+  app.get('/:id', (req, res, next) => {
+    Movie.findOne({where: {id: req.params.id}})
+        .then(movie => {
+            if (movie) {
+                return res.json(movie)
+            }
+            return res.status(404).end()
+        })
+        .catch(next)
+  })
+
+  app.patch('/:id', (req, res, next) => {
+    Movie.findOne({where: {id: req.params.id}})
+        .then(movie => {
+            if (movie) {
+                return movie.update(req.body)
+                    .then(movie => res.json(movie))
+            }
+            return res.status(404).end()
+        })
+        .catch(next)
+  })
+
+  app.delete('/:id', (req, res, next) => {
+    Movie.destroy({where: {id: req.params.id}})
+        .then(movie => {
+            if (movie) {
+                res.send(`Movie with Id:${req.params.id} deleted from the database`)
+            }
+            res.status(404).send("Movie does not exist in the db.")
+        })
+        .catch(next)
+  })
+  
+  app.listen(port, () => console.log("listening on port " + port))
 
